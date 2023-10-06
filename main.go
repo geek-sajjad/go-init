40bc6abd903f21430efe4ccbfad4566d5992e8e2
Sajad Sohrabi <sajjad.sohrabi2@gmail.com>
2023-10-06 13:19:29 +0330

    commit 1

--- main.go
+++ main.go
@@ -1,37 +1,78 @@
 package main
 
-import "fmt"
-
 func main() {
-	// var x int
-	// x = 10
+	// variables
 
-	// y := 20
+	// Explicit define
+	// var age int //Declare and integer variable
 
-	// z := x + y
+	// age = 30
 
-	// const test = "testing"
+	// Type Inference
+	// name := "my name"
 
-	// fmt.Println(z)
+	// isValid := true
 
-	// fmt.Println(add(1, 2))
+	// var count int // 'count' is assigned the zero value for integers, which is 0
 
-	// if x > 5 {
+	// var isActive bool // 'isActive' is assigned the zero value for booleans, which is false
 
+	// Multiple Variable Declaration
+
+	// var a, b, c int
+
+	// a, b, c = 1, 2, 3
+
+	// Constants
+
+	// const pi = 3.14
+
+	// if statements
+
+	// age2 := 25
+
+	// if age2 >= 18 {
+	// 	fmt.Println("You are an adult")
 	// } else {
-
+	// 	fmt.Println("You are a minor")
 	// }
-	var array [3]int
-	array[0] = 2
 
-	slice := []int{1, 2, 45, 3}
+	// For loops
 
-	for i := 0; i < 4; i++ {
-		fmt.Println(slice[i])
-	}
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
 
-}
+	// WHile loop
+	// count2 := 0
+	// for count2 < 5 {
+	// 	fmt.Println(count2)
+	// 	count2++
+	// }
 
-func add(a, b int) int {
-	return a + b
+	// for index, value := range collection {
+	// 	// Code to execute for each item in the collection
+	// }
+
+	// names := []string{"Alice", "Bob", "Charlie"}
+
+	// for index, name := range names {
+	// 	fmt.Println(index, name)
+	// }
+
+	// Arrays
+
+	// var myArray [size]Type
+
+	// var myArray [3]int
+	// for i := 0; i < len(myArray); i++ {
+	// 	fmt.Println(myArray[i])
+	// }
+
+	// for i, value := range myArray {
+	// 	fmt.Println(i, value)
+	// }
+
+	// When you pass an array to a function, it's passed by value, meaning a copy of the array is made. To avoid copying large arrays, you can pass a pointer to the array or use slices instead
+
 }
